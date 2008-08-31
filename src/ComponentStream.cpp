b79f3acdaf572f75d006b180872e74c9abcdc5a9
Alexander Saltykov <holycheater@gmail.com>
2008-08-31 20:16:44 +0600

    Stream authentication as a jabber server external component

--- src/ComponentStream.cpp
+++ src/ComponentStream.cpp
@@ -1,108 +1,256 @@
+/*
+ * ComponentStream.cpp - Jabber component stream (jabber:component:accept)
+ * Copyright (C) 2008  Alexander Saltykov
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 #include "ComponentStream.h"
-#include "ComponentProtocol.h"
 
 #include "xmpp.h"
 #include "bytestream.h"
-#include "securestream.h"
+#include "parser.h"
+
+#include <QCryptographicHash>
+#include <QXmlStreamWriter>
 
 namespace XMPP {
-
 
 class ComponentStream::Private
 {
 	public:
+		/* server connector (dns-lookups, connect) */
 		AdvancedConnector *connector;
-		ByteStream *bs;
-		ComponentProtocol protocol;
 
+		/* Connection socket stream */
+		ByteStream *bs;
+
+		/* component Jabber-ID */
 		Jid jid;
-		QString secret;
+
+		/* incoming data parser object */
+		Parser parser;
+
+		/* server component secret (password) */
+		QByteArray secret;
+
+		/* session Id, this will be told by server in the opening stream */
+		QByteArray sessionId;
+
+		/* connection status from ComponentStream::ConnectionStatus */
+		int connectionStatus;
 };
 
 ComponentStream::ComponentStream(AdvancedConnector *connector, QObject *parent)
-	: Stream(parent)
+	: QObject(parent)
 {
 	d = new Private;
+	d->connectionStatus = Disconnected;
 
 	d->connector = connector;
 	QObject::connect( d->connector, SIGNAL( connected() ), SLOT( cr_connected() ) );
 	QObject::connect( d->connector, SIGNAL( error() ), SLOT( cr_error() ) );
-};
+}
 
 ComponentStream::~ComponentStream()
 {
 	delete d;
 }
 
+/**
+ * Connects to the JID domain host to the specified port with secret
+ *
+ * @param jid		jabber-id
+ * @param port		server port
+ * @param secret	server secret for component
+ */
 void ComponentStream::connectToServer(const Jid& jid, quint16 port, const QString& secret)
 {
-	// TODO : use the parameters it receives.
 	d->jid = jid;
-	d->secret = secret;
+	d->secret = secret.toUtf8();
 
-	d->protocol.setJid(d->jid);
 	d->connector->setOptHostPort(jid.domain(), port);
 	d->connector->connectToServer( jid.domain() );
 }
 
-QDomDocument& ComponentStream::doc() const
-{
-
-}
-
+/**
+ *
+ * @return base stream namespace
+ */
 QString ComponentStream::baseNS() const
 {
 	return NS_COMPONENT;
 }
 
-bool ComponentStream::old() const
-{
-	return false;
-}
-
+/**
+ * Close the stream and connection
+ */
 void ComponentStream::close()
 {
-	// TODO
+	write("</stream:stream>");
 }
 
-bool ComponentStream::stanzaAvailable() const
+/**
+ * Process events from incoming stream
+ *
+ * @param event		incoming event
+ */
+void ComponentStream::processEvent(const Parser::Event& event)
 {
-	// TODO
+	qDebug() << "[CS]" << "processEvent()" << "type" << event.type();
+	switch ( event.type() ) {
+		case Parser::Event::DocumentClose:
+			qDebug() << "[CS] we are kicked off";
+			close();
+			break;
+		case Parser::Event::Element:
+			qDebug() << "[CS] we've got an element (stanza) here";
+			/* TODO: process stream:error */
+			processStanza(event);
+			break;
+		case Parser::Event::Error:
+			qDebug() << "[CS] whoops.. error occured";
+			close();
+			break;
+	}
 }
 
-Stanza ComponentStream::read()
-{
-	// TODO
-}
-
-void ComponentStream::write(const Stanza& stanza)
-{
-	// TODO
-}
-
-int ComponentStream::errorCondition() const
-{
-	// TODO
-}
-
-QString ComponentStream::errorText() const
-{
-	// TODO
-}
-
-QDomElement ComponentStream::errorAppSpec() const
-{
-	// TODO
-}
-
+/**
+ * Process next step for the server authentication process
+ */
 void ComponentStream::processNext()
 {
-	// TODO
+	switch ( d->connectionStatus ) {
+		case Disconnected:
+			qDebug() << "[CS]" << "processNext() in 'Disconnected' state. This should not happen";
+			break;
+		case InitIncomingStream: /* we need to process incoming stream initiation */
+			recv_stream_open();
+			break;
+		case RecvHandshakeReply: /* we need to receive handshake reply */
+			recv_handshake_reply();
+		case Connected: /* we need to process various events while we're connected. */
+		{
+			Parser::Event event = d->parser.readNext();
+			while ( !event.isNull() ) {
+				processEvent(event);
+				event = d->parser.readNext();
+			}
+			break;
+		}
+		default:
+			break;
+	}
 }
 
-void ComponentStream::bs_bytesWritten(int size)
+/**
+ * Process incoming stanza (first-level element)
+ * @param event		parser event
+ */
+void ComponentStream::processStanza(const Parser::Event& event)
 {
-	qDebug() << "[CS]" << "Bytestream bytesWritten:" << size;
+	if ( event.qualifiedName() == "message" ) {
+		qDebug() << "[CS]" << "incoming message";
+	} else if ( event.qualifiedName() == "iq" ) {
+		qDebug() << "[CS]" << "incoming info/query";
+	} else if ( event.qualifiedName() == "presence" ) {
+		qDebug() << "[CS]" << "presence info";
+	}
+}
+
+/**
+ * Handle incoming stream initiation
+ */
+void ComponentStream::recv_stream_open()
+{
+	Parser::Event event = d->parser.readNext();
+	d->sessionId = event.attributes().value("id").toUtf8();
+
+	/* stream accepted, next step - send the handshake */
+	d->connectionStatus = SendHandshake;
+	send_handshake();
+}
+
+/**
+ * Handle handshake reply
+ */
+void ComponentStream::recv_handshake_reply()
+{
+	Parser::Event event = d->parser.readNext();
+	if ( event.qualifiedName()=="handshake" && !event.attributes().count() ) {
+		qDebug() << "[CS]" << "Handshaking success";
+	} else {
+		qDebug() << "[CS]" << "Error!" << "Handshake failed";
+		close();
+	}
+
+	/* We have no errors */
+	d->connectionStatus = Connected;
+	emit connected();
+	processNext();
+}
+
+/**
+ * Initiate outgoing stream
+ */
+void ComponentStream::send_stream_open()
+{
+	QByteArray data;
+	QXmlStreamWriter xmlStream(&data);
+	xmlStream.writeStartDocument();
+	xmlStream.writeStartElement("stream:stream");
+	xmlStream.writeAttribute("xmlns:stream", NS_ETHERX);
+	xmlStream.writeDefaultNamespace(NS_COMPONENT);
+	xmlStream.writeAttribute( "to", d->jid.domain() );
+
+	/* dirty hack, cause QXmlStreamWriter can't close an opening element without appending closing tag */
+	data += ">";
+
+	write(data);
+	/* we sent stream open instruction, so server need to open the incoming stream */
+	d->connectionStatus = InitIncomingStream;
+}
+
+/**
+ * Send a handshake to the server: sha1(sessionID + secret)
+ */
+void ComponentStream::send_handshake()
+{
+	QByteArray hash = QCryptographicHash::hash(d->sessionId + d->secret, QCryptographicHash::Sha1).toHex();
+
+	QByteArray data;
+	QXmlStreamWriter writer(&data);
+	writer.writeStartElement("handshake");
+	writer.writeCharacters(hash);
+	writer.writeEndElement();
+	write(data);
+
+	/* we've sent handshake element, we need to get ack/error reply from the server */
+	d->connectionStatus = RecvHandshakeReply;
+}
+
+/**
+ * Write data to the outgoing stream
+ *
+ * @param data		XML data
+ */
+void ComponentStream::write(const QByteArray& data)
+{
+	qDebug() << "[CS]" << "-send-" << data;
+	d->bs->write(data);
 }
 
 void ComponentStream::bs_closed()
@@ -120,24 +268,24 @@ void ComponentStream::bs_readyRead()
 	QByteArray data = d->bs->read();
 	qDebug() << "[CS]" << "-recv-" << data;
 
-	d->protocol.addIncomingData(data);
+	d->parser.appendData(data);
+	while ( d->parser.unprocessed().size() > 0 ) {
+		processNext();
+	}
 }
 
 void ComponentStream::cr_connected()
 {
-	qDebug() << "[CS]" << "Connector connected";
-
 	d->bs = d->connector->stream();
 
-	QByteArray spare = d->bs->read();
-	qDebug() << "[CS]" << "'spare' size" << spare.size();
-
+	QObject::connect( d->bs, SIGNAL( connectionClosed() ), SIGNAL( disconnected() ) );
 	QObject::connect( d->bs, SIGNAL( connectionClosed() ), SLOT( bs_closed() ) );
-	QObject::connect( d->bs, SIGNAL( bytesWritten(int) ), SLOT( bs_bytesWritten(int) ) );
 	QObject::connect( d->bs, SIGNAL( error(int) ), SLOT( bs_error(int) ) );
 	QObject::connect( d->bs, SIGNAL( readyRead() ), SLOT( bs_readyRead() ) );
 
-	processNext();
+	/* we've connected to the server, so we need to initiate communications */
+	d->connectionStatus = InitOutgoingStream;
+	send_stream_open();
 }
 
 void ComponentStream::cr_error()
@@ -145,5 +293,13 @@ void ComponentStream::cr_error()
 	qDebug() << "[CS]" << "Connector error";
 }
 
+/**
+ * Send keep-alive data (whitespace)
+ */
+void ComponentStream::send_keepalive()
+{
+	write(" ");
+}
 
-} // end of namespace XMPP
+
+} /* end of namespace XMPP */
--- src/ComponentStream.h
+++ src/ComponentStream.h
@@ -1,54 +1,78 @@
+/*
+ * ComponentStream.h - Jabber component stream (jabber:component:accept)
+ * Copyright (C) 2008  Alexander Saltykov
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 #ifndef COMPONENTSTREAM_H_
 #define COMPONENTSTREAM_H_
 
-#include "xmpp_stream.h"
-
-#include <QDomDocument>
+#include "parser.h"
 #include <QObject>
-#include <QString>
+
+#ifndef NS_ETHERX
+#define NS_ETHERX "http://etherx.jabber.org/streams"
+#endif
+#define NS_COMPONENT "jabber:component:accept"
 
 namespace XMPP {
 
 class AdvancedConnector;
+class Jid;
 
-
-class ComponentStream : public Stream
+class ComponentStream : public QObject
 {
 	Q_OBJECT
+
+	enum ConnectionStatus { Disconnected, InitOutgoingStream, InitIncomingStream, SendHandshake, RecvHandshakeReply, Connected };
 
 	public:
 		ComponentStream(AdvancedConnector *connector, QObject *parent = 0);
 		~ComponentStream();
 
 		void connectToServer(const Jid& jid, quint16 port, const QString& secret);
-
-		QDomDocument& doc() const;
-		QString baseNS() const;
-		bool old() const;
-
 		void close();
-		bool stanzaAvailable() const;
-		Stanza read();
-		void write(const Stanza& stanza);
 
-		int errorCondition() const;
-		QString errorText() const;
-		QDomElement errorAppSpec() const;
+		QString baseNS() const;
+	signals:
+		void connected();
+		void disconnected();
 	private:
+		void processEvent(const Parser::Event& event);
 		void processNext();
+		void processStanza(const Parser::Event& event);
+		void recv_stream_open();
+		void recv_handshake_reply();
+		void send_stream_open();
+		void send_handshake();
+		void write(const QByteArray& data);
 	private slots:
-		void bs_bytesWritten(int size);
 		void bs_error(int errno);
 		void bs_readyRead();
 		void bs_closed();
 		void cr_connected();
 		void cr_error();
+		void send_keepalive();
 	private:
 		class Private;
 		Private *d;
 };
 
 
-} // end of namespace XMPP
+} /* end of namespace XMPP */
 
 #endif /* COMPONENTSTREAM_H_ */

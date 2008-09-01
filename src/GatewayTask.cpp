e6718caf4fd1192d6f340915039a8c9f0792f862
Alexander Saltykov <holycheater@gmail.com>
2008-09-02 02:43:37 +0600

    Fix Gateway task comments in the head of file, so they won't be parsed by doxygen

--- src/GatewayTask.cpp
+++ src/GatewayTask.cpp
@@ -1,4 +1,4 @@
-/**
+/*
  * GatewayTask.cpp - Handles tasks from jabber clients (register/status/message/etc)
  * Copyright (C) 2008  Alexander Saltykov
  *
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- **/
+ */
 
 #include "GatewayTask.h"
 
